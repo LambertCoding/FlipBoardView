a04244a3a68595eccf607565f1da1639ce1ebdc8
baylor <229868243@qq.com>
2019-05-16 22:43:24 +0800

    Initial commit

---
+++ app/src/main/java/com/gitbub/flipboard/FlipBoardView.java
@@ -0,0 +1,232 @@
+package com.gitbub.flipboard;
+
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.*;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * 仿写红板报动画的View
+ *
+ * @author yu
+ */
+public class FlipBoardView extends View {
+    // 翻起的一半,Y轴方向旋转角度
+    private float degreeY;
+    // 不变的那一半，Y轴方向旋转角度
+    private float fixDegreeY;
+    // Z轴方向（平面内）旋转的角度
+    private float degreeZ;
+
+    // 最终执行完动画后，图片翻起的角度
+    private int finalAngle;
+
+    private Paint paint;
+    private Bitmap bitmap;
+    private Camera camera;
+    private AnimatorSet animatorSet;
+
+    public FlipBoardView(Context context) {
+        this(context, null);
+    }
+
+    public FlipBoardView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FlipBoardView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, @Nullable AttributeSet attrs) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.FlipBoardView);
+        BitmapDrawable drawable = (BitmapDrawable) a.getDrawable(R.styleable.FlipBoardView_fbv_bitmap);
+        finalAngle = a.getInteger(R.styleable.FlipBoardView_fbv_angleY, 30);
+        a.recycle();
+
+        if (drawable != null) {
+            bitmap = drawable.getBitmap();
+        } else {
+            bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.flip_board);
+        }
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        camera = new Camera();
+
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        float newZ = -displayMetrics.density * 8;
+        camera.setLocation(0, 0, newZ);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int bitmapWidth = bitmap.getWidth();
+        int bitmapHeight = bitmap.getHeight();
+
+        int paddingStart = getPaddingStart();
+        int paddingTop = getPaddingTop();
+        int paddingEnd = getPaddingEnd();
+        int paddingBottom = getPaddingBottom();
+
+        setMeasuredDimension(resolveSize(bitmapWidth + paddingStart + paddingEnd, widthMeasureSpec),
+                resolveSize(bitmapHeight + paddingTop + paddingBottom, heightMeasureSpec));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int bitmapCenterX = bitmap.getWidth() >> 1;
+        int bitmapCenterY = bitmap.getHeight() >> 1;
+        int centerX = getWidth() >> 1;
+        int centerY = getHeight() >> 1;
+
+        // 分为两步：1，画3D翻起的一半  2，画正常状态的一半，具体又分几步：
+        // 先移动画布到中心，让camera旋转效果居中
+        // 旋转画布
+        // 裁切需要绘制的范围
+        // 运用camera旋转效果
+        canvas.save();
+        camera.save();
+
+        canvas.translate(centerX, centerY);
+        canvas.rotate(-degreeZ);
+
+        camera.rotateY(degreeY);
+        camera.applyToCanvas(canvas);
+        camera.restore();
+
+        // 注意坐标系已经移动到中心了
+        canvas.clipRect(0, -centerY, centerX, centerY);
+
+        canvas.rotate(degreeZ);
+        canvas.translate(-centerX, -centerY);
+
+        canvas.drawBitmap(bitmap, centerX - bitmapCenterX, centerY - bitmapCenterY, paint);
+        canvas.restore();
+
+
+        // 画另一半
+        canvas.save();
+        camera.save();
+
+        canvas.translate(centerX, centerY);
+        canvas.rotate(-degreeZ);
+
+        camera.rotateY(fixDegreeY);
+        camera.applyToCanvas(canvas);
+        camera.restore();
+
+        canvas.clipRect(-centerX, -centerY, 0, centerY);
+
+        canvas.rotate(degreeZ);
+        canvas.translate(-centerX, -centerY);
+
+        canvas.drawBitmap(bitmap, centerX - bitmapCenterY, centerY - bitmapCenterY, paint);
+        canvas.restore();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onVisibilityChanged(@NonNull View changedView, int visibility) {
+        super.onVisibilityChanged(changedView, visibility);
+        if (visibility == VISIBLE) {
+            start();
+        } else {
+            stop();
+        }
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        start();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        stop();
+    }
+
+    public void start() {
+        if (animatorSet == null) {
+            ValueAnimator startAnim = ValueAnimator.ofFloat(0, -finalAngle);
+            startAnim.setDuration(600);
+            startAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    setDegreeY((Float) animation.getAnimatedValue());
+                }
+            });
+            startAnim.setStartDelay(200);
+
+            ValueAnimator middleAnim = ValueAnimator.ofFloat(0, 270);
+            middleAnim.setDuration(1000);
+            middleAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    setDegreeZ((Float) animation.getAnimatedValue());
+                }
+            });
+            middleAnim.setStartDelay(300);
+
+            ValueAnimator endAnim = ValueAnimator.ofFloat(0, finalAngle);
+            endAnim.setDuration(600);
+            endAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    setFixDegreeY((Float) animation.getAnimatedValue());
+                }
+            });
+            endAnim.setStartDelay(200);
+
+            animatorSet = new AnimatorSet();
+            animatorSet.playSequentially(startAnim, middleAnim, endAnim);
+
+            animatorSet.start();
+        } else if (!animatorSet.isStarted()) {
+            animatorSet.start();
+        }
+    }
+
+    public void stop() {
+        if (animatorSet != null) {
+            animatorSet.removeAllListeners();
+            animatorSet.cancel();
+            animatorSet = null;
+        }
+    }
+
+    public void setDegreeY(float degreeY) {
+        this.degreeY = degreeY;
+        invalidate();
+    }
+
+    public void setFixDegreeY(float fixDegreeY) {
+        this.fixDegreeY = fixDegreeY;
+        invalidate();
+    }
+
+    public void setDegreeZ(float degreeZ) {
+        this.degreeZ = degreeZ;
+        invalidate();
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
+        invalidate();
+    }
+}
